9992b6b7bef1fac259f170eff218433b1d5d76cd
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-10 20:48:20 +0100

    add changes

--- src/assets/Components/FrontPage/Welcome.js
+++ src/assets/Components/FrontPage/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Welcome = () => {
     return <div>
         <h3 className='fonth1Body '>Vestskovs Grillfest.</h3>
-        <h1 className='fonth1 phone:text-6xl tablet:text-9xl laptop:text-9xl desktop:text-9xl' >GNUFESTIVAL ER: <br /> LÆKKER MAD, <br /> GOD MUSIK, <br /> GODT SELSKAB <br /> OG FEST.</h1>
+        <h1 className='fonth1 phone:text-6xl tablet:text-9xl laptop:text-9xl desktop:text-9xl' >GRILLFESTIVAL ER: <br /> LÆKKER MAD, <br /> GOD MUSIK, <br /> GODT SELSKAB <br /> OG FEST.</h1>
     </div>;
 };
 
