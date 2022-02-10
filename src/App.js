352b41c631e6f587e67f51b2b5e86817566eba6c
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-10 20:24:21 +0100

    Add changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import GnuLogo from './assets/Components/Header/GnuLogo';
 const App = () => {
     return (
         <div>
-            <div className='bg-gradient-to-r from-yellow-300 to-gray-400 '>
+            <div className='bg-gradient-to-r from-yellow-300 to-gray-400  h-[1000px]'>
                 <div className='p-3 flex items-center justify-between w-full bg-transparent fixed'>
                     <div className='flex items-center'>
                         <GnuLogo />
