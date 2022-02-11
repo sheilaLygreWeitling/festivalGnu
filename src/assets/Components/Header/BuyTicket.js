5fd8110e3ae34bcf03dc6dfceb208fd8790bea92
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-11 11:41:45 +0100

    add changes

--- src/assets/Components/Header/BuyTicket.js
+++ src/assets/Components/Header/BuyTicket.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { MdAddShoppingCart } from "react-icons/md";
 
+
 const BuyTicket = () => {
     return <div>
-        <button className='text-current flex items-center fonth1Body ' > <MdAddShoppingCart /> Køb din billet her</button>
+
+        <button className='text-current flex items-center fonth1Body ' onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLScL8U91vKNRZlvy214P9mlD3qdjrOo7tjbo3jDb9WyOGuSTZg/viewform")}> <MdAddShoppingCart /> Køb din billet her</button>
+
     </div>;
 };
 
