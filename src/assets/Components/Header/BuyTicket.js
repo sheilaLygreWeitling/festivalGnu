94ca6225fa5ff9f330baf69990fa954e8ba5311a
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-20 12:48:45 +0100

    Add test

--- src/assets/Components/Header/BuyTicket.js
+++ src/assets/Components/Header/BuyTicket.js
@@ -7,7 +7,10 @@ const BuyTicket = () => {
 
         <button className='text-current flex items-center fonth1Body text-5xl' onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLScL8U91vKNRZlvy214P9mlD3qdjrOo7tjbo3jDb9WyOGuSTZg/viewform")}> Køb din billet her</button>
         <div className='border border-black'>
-
+            <form action="post" netflify name="test">
+                <input type="email" name="email" id="" />
+                <button type="submit">send</button>
+            </form>
         </div>
 
     </div>;
