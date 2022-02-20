93e0634e81a2d72bedc145ae0ac2e75d05d7503a
Sheila Lygre Weitling <sheila-l-w@hotmail.com>
2022-02-20 13:09:36 +0100

    test editing form method to action

--- src/assets/Components/Header/BuyTicket.js
+++ src/assets/Components/Header/BuyTicket.js
@@ -7,7 +7,8 @@ const BuyTicket = () => {
 
         <button className='text-current flex items-center fonth1Body text-5xl' onClick={() => window.open("https://docs.google.com/forms/d/e/1FAIpQLScL8U91vKNRZlvy214P9mlD3qdjrOo7tjbo3jDb9WyOGuSTZg/viewform")}> Køb din billet her</button>
         <div className='border border-black'>
-            <form action="post" netflify name="test" data-netlify="true">
+            <form method="post" name="form-test" data-netlify="true">
+                <input type="hidden" name="form-name" value="form-test" />
                 <input type="email" name="email" id="" />
                 <button type="submit">send</button>
             </form>
